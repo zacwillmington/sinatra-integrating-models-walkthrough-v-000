b70f8a12feaac5ff1ddb9e9167b947519dd966c9
Zac Willmington <zac.willmington@gmail.com>
2018-05-10 16:33:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ class App < Sinatra::Base
   post '/' do
     text_from_user = params[:user_text]
     @analyzed_text = TextAnalyzer.new(text_from_user)
+    binding.pry
     erb :results
   end
 end
